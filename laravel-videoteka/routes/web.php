de1e6a17ec0a09f0c5bee110d171fd3bece1f109
Aleksandar Dobrinic <aleksandar.dobrinic@predavaci.algebra.hr>
2024-09-09 21:06:35 +0200

    Prices index

--- laravel-videoteka/routes/web.php
+++ laravel-videoteka/routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
+use App\Models\Price;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/prices', function () {
+
+    $prices = Price::all();
+
+    return view('prices.index', [
+        'prices' => $prices
+    ]);
+});
+
 Route::post('/prices', function () {
     return view('welcome');
 });
+
+// ORM - Object Relational Mapping
